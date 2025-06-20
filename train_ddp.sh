7975d2b097dd59300b61e86c05fb5144924e8cd0
Po-An Shou <55120389+sarkar0613@users.noreply.github.com>
2025-06-21 00:58:58 +0800

    Update train_ddp.sh

--- train_ddp.sh
+++ train_ddp.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 NUM_GPUS=$(nvidia-smi -L | wc -l)
-mkdir -p ./results/ddp
 
 python -m torch.distributed.launch \
   --nproc_per_node=$NUM_GPUS \
-  --master_port=29500 \
+  --master_port=58889 \
   src/main.py \
   --result_dir ./results/ddp \
-  --verify_path /absolute/path/to/Innolux_verify.pt \
-  --enroll_path /absolute/path/to/Innolux_enroll.pt \
+  --verify_path /absolute/path/to/Innolux_verify_fe.pt \
+  --enroll_path /absolute/path/to/Innolux_enroll_fe.pt \
+  --batch_size 256 \
   --epochs 20 \
-  --batch_size 64 \
-  --learning_rate 1e-4 \
-  --use_ddp True
+  --use_stn True \
+  --use_ddp True \
+  --dist_url tcp://localhost:58889
