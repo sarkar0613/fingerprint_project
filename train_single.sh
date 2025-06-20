c4f0c4d79ef987ad3c700f0b114d73c1a5262d84
Po-An Shou <55120389+sarkar0613@users.noreply.github.com>
2025-06-21 00:59:30 +0800

    Update train_single.sh

--- train_single.sh
+++ train_single.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-mkdir -p ./results/single_gpu
-
 python src/main.py \
   --result_dir ./results/single_gpu \
-  --verify_path /absolute/path/to/Innolux_verify.pt \
-  --enroll_path /absolute/path/to/Innolux_enroll.pt \
+  --verify_path /absolute/path/to/Innolux_verify_fe.pt \
+  --enroll_path /absolute/path/to/Innolux_enroll_fe.pt \
+  --batch_size 128 \
   --epochs 20 \
-  --batch_size 64 \
-  --learning_rate 1e-4 \
+  --use_stn True \
   --use_ddp False
